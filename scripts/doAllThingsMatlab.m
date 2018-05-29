ca10b07f65e6031a137d1636973d87ee2952816e
Tobias Hornberger <tobi@ISAS-Rechner.com>
2018-05-29 11:03:19 +0200

    add RawMeasurements component

--- scripts/doAllThingsMatlab.m
+++ scripts/doAllThingsMatlab.m
@@ -1,4 +1,12 @@
+% Usage: 
+% 1. add nessesary things to path -addpath(genpath('matlab') ?
+% 2. navigate to folder with data you want to process
+% 3. optional: change files variable to fit your input csvs.
+% >> doAllThingsMatlab
+
 disp('starting script')
+
+%Set magic Parameters, so the tracking works well
 allParam=getDefaultParam([30;0]);
 allParam.score.looseStep=30;
 allParam.association.useOrientation=false;
@@ -7,17 +15,26 @@ allParam.meas.PositionCov=2000*eye(2);
 allParam.association.tryToUseMex=false;
 disp('parameters set!')
 
-files = dir('*_data.csv');
+%files = dir('*_data.csv');
+files = dir('*case3.csv');
 i = 1;
 for file = files'
     trackHistory=tracksortAlgorithm([0,2450;0,1500],1300,1450,allParam,file.name);
+    %These numbers in this command should be fitted for the input data, but
+    %same camera setup should be able to use the same ones.
     saving = strcat("Trackhistory_", file.name(1:end-4));
-    saving = strcat(saving, ".mat")
-    save(saving, "trackHistory")
+    savingFile = strcat(saving, ".mat");
+    %save trackhistory to file
+    save(savingFile, "trackHistory");
     i = i + 1;
-    if i == 4
-        a = 42;
+    A = transpose(trackHistory(1).RawMeasurements);
+    for it = 2:size(trackHistory,2)
+        B = transpose(trackHistory(it).RawMeasurements);
+        A = horzcat(A,B);
     end
+    disp(A)
+    %csvwrite(strcat(saving,"_RawMeas.csv") ,trackHistory(1).RawMeasurements)
+    
 end
     
 disp('finished')
