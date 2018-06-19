c879262cc3a4ed327b40e8c7f5b033bd45472d8c
Tobias Hornberger <saibot1207@googlemail.com>
2018-06-19 18:04:46 +0200

    add pandas read data for real test data with nans

--- implementation/loadDataExample.py
+++ implementation/loadDataExample.py
@@ -1,15 +1,9 @@
-import tensorflow as tf
-import tensorflow_hub as hub
-import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
-import re
 import random
 from sklearn.model_selection import train_test_split
 import logging
-
-load = False
 
 
 # featureSize = 5
@@ -98,6 +92,96 @@ def loadCSVtoNpy(inputFile='/home/tobi/Projects/KIT/MasterarbeitTobias/data/Grou
 	np.save(outputfile, x)
 
 
+def _validateDF(dataFrame, featureSize=5):
+
+	df = dataFrame
+	threshold = df.shape[0] - featureSize - 1 # has to fit at least 1 datapoint
+	df = df.dropna(axis=1, thresh=threshold)
+
+	# vals_clean = vals[~np.isnan(vals)]
+
+	#TODO: zusammenhängende werte?
+
+	return df
+
+
+def _removeNans(array):
+	arr1d = array
+	notnans = np.flatnonzero(~np.isnan(arr1d))
+	if notnans.size:
+		trimmed = arr1d[notnans[0]: notnans[-1] + 1]  # slice from first not-nan to the last one
+	else:
+		trimmed = np.zeros(0)
+
+	return trimmed
+
+
+def loadRawMeas(inputFile='/home/tobi/Projects/KIT/MasterarbeitTobias/data/Trackhistory_02_data_RawMeas.csv',
+                 outputfile='in.npy', featureSize=5):
+	"""loads real(!) data from a csv file and generates a set of features and labels from it."""
+
+	# dt = np.dtype([('features', float, (2 * featureSize,)), ('labels', float, (2,))])
+
+	df = pd.read_csv(inputFile)
+	df = _validateDF(df, featureSize)
+
+	numberTracks = (df.shape[1] - 2) / 2
+
+	assert numberTracks == int(numberTracks)
+
+	numberTracks = int(numberTracks)
+
+	columnNames = genColumnNames(featureSize)
+	columnNames.append('LabelX')
+	columnNames.append('LabelY')
+
+	data = []
+
+	for i in range(numberTracks):
+		a = df.iloc[:, (2 * i)].values
+		b = df.iloc[:, (2 * i + 1)].values
+
+		# a = a[~np.isnan(a)]   #Remove nans from columns
+		# b = b[~np.isnan(b)]   #Remove NaNs from Columns
+
+		a = _removeNans(a)
+		b = _removeNans(b)
+
+		assert len(a) == len(b)
+
+		featureCount = len(a) - featureSize
+		for k in range(featureCount):
+			temp = {}
+			temp['features'] = np.append(a[k:(k+ featureSize)], b[k:(k+featureSize)])
+			temp['labels'] = np.append(a[k + featureSize], b[k+featureSize])
+			data.append(temp)
+
+	# newDf = pd.DataFrame(columns=columNames)
+	dataFrameList = []
+
+	for elem in data:
+		# print(elem)
+		tempdict = {columnNames[k]: elem['features'][k] for k in range(2 * featureSize)}
+		for i in [-2, -1]:
+			tempdict[columnNames[i]] = elem['labels'][i]
+
+		dataFrameList.append(pd.DataFrame(tempdict, index=[0]))
+
+	newDf = pd.concat(dataFrameList, ignore_index=True)
+
+	labelDf = newDf[['LabelX', 'LabelY']].copy()
+	featureDf = newDf.drop(['LabelX', 'LabelY'], axis=1)
+
+	trainFeatures, testFeatures, trainLabels, testLabels = train_test_split(featureDf, labelDf, test_size=0.1)
+
+	print(testFeatures)
+	print(testLabels)
+
+	return newDf
+
+	# return (trainFeatureDict, trainLabels), (testFeatureDict, testLabels)
+
+
 def loadData(featureSize=5):
 	"""generates data in the right format from csv files and returns a
 	dataset as (train_features, train_labels), (test_features, test_labels)."""
@@ -183,12 +267,10 @@ def loadFakeData(featureSize=5, numberOfLines=100):
 			logging.info("Data Generation progress: %i / %i" % (len(dataArray), numberOfLines))
 			# print(len(dataArray))
 
-
 	# data = dataArray.pop()
 	# for i in dataArray:  # TODO: Maybe improve performance here?
 	# 	data = np.append(data, i)
 	data = np.concatenate(dataArray)
-
 
 	trainFeatures, testFeatures, trainLabels, testLabels = train_test_split(data['features'], data['labels'], test_size=0.1)
 
