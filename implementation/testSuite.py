f655742ca67538e588479630308660473f6e8e90
Tobias Hornberger <saibot1207@googlemail.com>
2018-07-03 15:20:30 +0200

    prepare merge to master

--- implementation/testSuite.py
+++ implementation/testSuite.py
@@ -3,6 +3,8 @@ import loadDataExample as ld
 import os
 from numpy import *
 import logging
+import pandas as pd
+
 
 def test1():
 	print("Running testSuite")
@@ -31,15 +33,48 @@ def test2():
 	df = ld.prepareRawMeas('/home/hornberger/Projects/MasterarbeitTobias/data/experiment01/07_data_trackHistory_NothingDeleted.csv', 5)
 	print(df)
 
-def test3():
+def test3(loading=False):
 	print("loading data")
-	(X_train1, y_train1), (X_test1, y_test1) = ld.loadRawMeas('/home/hornberger/Projects/MasterarbeitTobias/data/experiment04', 5, 0.1)
-	(X_train2, y_train2), (X_test2, y_test2) = ld.loadFakeDataPandas(5, 500, 0.1)
 
-	print(type(X_test1))
+	if loading == False:
+		(X_train1, y_train1), (X_test1, y_test1) = ld.loadRawMeas('/home/hornberger/Projects/MasterarbeitTobias/data/experiment01', 5, 0.1)
+		(X_train2, y_train2), (X_test2, y_test2) = ld.loadFakeDataPandas(5, 500, 0.1)
+		with pd.HDFStore('data.h5') as store:
+			store['xtrain1'] = X_train1
+			store['ytrain1'] = y_train1
 
+			store['xtest1'] = X_test1
+			store['ytest1'] = y_test1
+
+			store['xtrain2'] = X_train2
+			store['ytrain2'] = y_train2
+
+			store['xtest2'] = X_test2
+			store['ytest2'] = y_test2
+	else:
+		try:
+			logging.info("loading data from store")
+
+			with pd.HDFStore('data.h5') as store:
+				X_train1 = store['xtrain1']
+				y_train1 = store['ytrain1']
+
+				X_test1 = store['xtest1']
+				y_test1 = store['ytest1']
+
+				X_train2 = store['xtrain2']
+				y_train2 = store['ytrain2']
+
+				X_test2 = store['xtest2']
+				y_test2 = store['ytest2']
+		except:
+			# logging.error("Error while loading from pickled data")
+			logging.error("Error while loading from stored data")
+			exit()
+
+	print(X_train1[X_train1.isna().any(axis=1)]) #[X_train1.isnull().any(axis=1)])
 
 
 logging.getLogger().setLevel(logging.INFO)
 
-test3()
+test3(False)
