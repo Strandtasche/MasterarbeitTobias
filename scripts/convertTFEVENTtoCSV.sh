feb5efa5e375246e690cbfb908ebd30d28d72e16
Tobias Hornberger <saibot1207@googlemail.com>
2018-07-26 11:31:26 +0200

    correct convert script typo and remove unnessesary semicolons from tensorflowlog

--- scripts/convertTFEVENTtoCSV.sh
+++ scripts/convertTFEVENTtoCSV.sh
@@ -4,7 +4,7 @@ for i in **/*tfevents*; do # Whitespace-safe and recursive
     DIR=$(dirname "$i")
     if [[ $DIR != *eval ]]; then
         python ~/MasterarbeitTobias/scripts/exportTensorFlowLog.py $i $DIR
-        #mv "$DIR/scalar.csv" "$DIR/$(basename $i).csv"
+        mv "$DIR/scalars.csv" "$DIR/$(basename $i).csv"
     fi
 done
 
