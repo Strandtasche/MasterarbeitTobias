7b0e1e78c6f91687a60a16537375386ae92d4625
Tobias Hornberger <saibot1207@googlemail.com>
2018-07-31 17:03:41 +0200

    add script

---
+++ scripts/splitcsv.sh
@@ -0,0 +1,13 @@
+
+echo "file to be split: {$1}"
+
+numberColumns="$(head -1 $1 | sed 's/[^,]//g' | wc -c)"
+last=0
+if [ ! -d "$2" ]; then
+    echo "creating folder $2"
+    mkdir "$2"
+fi
+for i in `seq -f "%05g" 1 400 $numberColumns`; do
+    cut -d "," -f$((10#$i))-$((10#$i + 399)) $1 > "$2/${1: :-4}_$i.csv"
+done
+
