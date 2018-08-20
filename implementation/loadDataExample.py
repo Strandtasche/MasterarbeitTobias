21fd3827d2e7c13c68622635d3ffe349e26b785a
Tobias Hornberger <saibot1207@googlemail.com>
2018-08-20 16:58:00 +0200

    mini commit with small changes

--- implementation/loadDataExample.py
+++ implementation/loadDataExample.py
@@ -110,13 +110,14 @@ def _removeNans(array):
 
 	return trimmed
 
-# kopiert aus Stackoverflow thread "https://stackoverflow.com/questions/20677795/how-do-i-compute-the-intersection-point-of-two-lines-in-python"f
 
+# kopiert aus Stackoverflow thread "https://stackoverflow.com/questions/20677795/how-do-i-compute-the-intersection-point-of-two-lines-in-python"f
 def _line(p1, p2):
 	A = (p1[1] - p2[1])
 	B = (p2[0] - p1[0])
 	C = (p1[0]*p2[1] - p2[0]*p1[1])
 	return A, B, -C
+
 
 def _intersection(L1, L2):
 	D  = L1[0] * L2[1] - L1[1] * L2[0]
@@ -128,6 +129,7 @@ def _intersection(L1, L2):
 		return x,y
 	else:
 		return False
+
 
 def _distanceEu(p1, p2):
 	return ((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2) ** 0.5
