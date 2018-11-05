269acc9771a6f9e7a981bd4421fc369c5df2e50c
Tobias Hornberger <saibot1207@googlemail.com>
2018-11-05 11:15:04 +0100

    bashstart Nextstep scripts

--- scripts/bashStart-NextStep-18-11-01.sh
+++ scripts/bashStart-NextStep-18-11-01.sh
@@ -4,7 +4,7 @@ echo "Starting Execution"
 dt=$(date '+%d/%m/%Y %H:%M:%S');
 echo "$dt"
 
-modelFolder="/home/hornberger/MasterarbeitTobias/models/simulated/NextStep/NextStep-60kDecaySteps-01Layers/"
+modelFolder="/home/hornberger/MasterarbeitTobias/models/simulated/NextStep/NextStep-60kDecaySteps-01Layers"
 python DNNRegressor-Example.py --training --custom --augment --save /home/hornberger/MasterarbeitTobias/data/simulated/downsampled/h5/Spheres-FS5-Augm.h5 --hyperparams /home/hornberger/Projects/MasterarbeitTobias/implementation/Finetuning-Simulated/Simulated-NextStep-60kDecay-01Layers.json  --overrideModel $modelFolder  2>&1 | tee -a /home/hornberger/MasterarbeitTobias/logs/Finetuning-Simulated/60kDecaySteps-01layers.txt
 (cd $modelFolder && bash /home/hornberger/MasterarbeitTobias/scripts/convertTFEVENTtoCSV.sh delete)
 
@@ -13,7 +13,7 @@ dt=$(date '+%d/%m/%Y %H:%M:%S');
 echo "$dt"
 echo "finished hyperV1"
 
-modelFolder="/home/hornberger/MasterarbeitTobias/models/simulated/NextStep/NextStep-60kDecaySteps-05Layers/"
+modelFolder="/home/hornberger/MasterarbeitTobias/models/simulated/NextStep/NextStep-60kDecaySteps-05Layers"
 python DNNRegressor-Example.py --training --custom --augment --load /home/hornberger/MasterarbeitTobias/data/simulated/downsampled/h5/Spheres-FS5-Augm.h5 --hyperparams /home/hornberger/Projects/MasterarbeitTobias/implementation/Finetuning-Simulated/Simulated-NextStep-60kDecay-05Layers.json  --overrideModel $modelFolder  2>&1 | tee -a /home/hornberger/MasterarbeitTobias/logs/Finetuning-Simulated/60kDecaySteps-05layers.txt
 (cd $modelFolder && bash /home/hornberger/MasterarbeitTobias/scripts/convertTFEVENTtoCSV.sh delete)
 
@@ -22,7 +22,7 @@ echo "$dt"
 echo "finished hyperV2"
 
 modelFolder="/home/hornberger/MasterarbeitTobias/models/simulated/NextStep/NextStep-FS3"
-python DNNRegressor-Example.py --training --custom --augment --save /home/hornberger/MasterarbeitTobias/data/simulated/downsampled/h5/Spheres-FS3-Augm.h5 --hyperparams /home/hornberger/Projects/MasterarbeitTobias/implementation/Finetuning-Simulated/Simulated-NextStep-FS3.json --overrideModel $modelFolder  1>&1 | tee -a /home/hornberger/MasterarbeitTobias/logs/Finetuning-Simulated/NextStep-FS3.json
+python DNNRegressor-Example.py --training --custom --augment --save /home/hornberger/MasterarbeitTobias/data/simulated/downsampled/h5/Spheres-FS3-Augm.h5 --hyperparams /home/hornberger/Projects/MasterarbeitTobias/implementation/Finetuning-Simulated/Simulated-NextStep-FS3.json --overrideModel $modelFolder  2>&1 | tee -a /home/hornberger/MasterarbeitTobias/logs/Finetuning-Simulated/NextStep-FS3.json
 (cd $modelFolder && bash /home/hornberger/MasterarbeitTobias/scripts/convertTFEVENTtoCSV.sh delete)
 
 dt=$(date '+%d/%m/%Y %H:%M:%S');
@@ -31,7 +31,7 @@ echo "finished hyperV3"
 
 
 modelFolder="/home/hornberger/MasterarbeitTobias/models/simulated/NextStep/NextStep-FS10"
-python DNNRegressor-Example.py --training --custom --augment --save /home/hornberger/MasterarbeitTobias/data/simulated/downsampled/h5/Spheres-FS10-Augm.h5 --hyperparams /home/hornberger/Projects/MasterarbeitTobias/implementation/Finetuning-Simulated/Simulated-NextStep-FS10.json --overrideModel $modelFolder  1>&1 | tee -a /home/hornberger/MasterarbeitTobias/logs/Finetuning-Simulated/NextStep-FS10.json
+python DNNRegressor-Example.py --training --custom --augment --save /home/hornberger/MasterarbeitTobias/data/simulated/downsampled/h5/Spheres-FS10-Augm.h5 --hyperparams /home/hornberger/Projects/MasterarbeitTobias/implementation/Finetuning-Simulated/Simulated-NextStep-FS10.json --overrideModel $modelFolder  2>&1 | tee -a /home/hornberger/MasterarbeitTobias/logs/Finetuning-Simulated/NextStep-FS10.json
 (cd $modelFolder && bash /home/hornberger/MasterarbeitTobias/scripts/convertTFEVENTtoCSV.sh delete)
 
 dt=$(date '+%d/%m/%Y %H:%M:%S');
---
+++ scripts/bashStart-NextStep-18-11-03.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+echo "Starting Execution"
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+echo "$dt"
+
+modelFolder="/home/hornberger/MasterarbeitTobias/models/simulated/NextStep/NextStep-60kDecaySteps-10Layers"
+python DNNRegressor-Example.py --training --custom --augment --load /home/hornberger/MasterarbeitTobias/data/simulated/downsampled/h5/Spheres-FS5-Augm.h5 --hyperparams /home/hornberger/Projects/MasterarbeitTobias/implementation/Finetuning-Simulated/Simulated-NextStep-60kDecay-10Layers.json  --overrideModel $modelFolder  2>&1 | tee -a /home/hornberger/MasterarbeitTobias/logs/Finetuning-Simulated/60kDecaySteps-10layers.txt
+(cd $modelFolder && bash /home/hornberger/MasterarbeitTobias/scripts/convertTFEVENTtoCSV.sh delete)
+
+
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+echo "$dt"
+echo "finished hyperV1"
+
+modelFolder="/home/hornberger/MasterarbeitTobias/models/simulated/NextStep/NextStep-60kDecaySteps-10Layers-RegL1-A"
+python DNNRegressor-Example.py --training --custom --augment --load /home/hornberger/MasterarbeitTobias/data/simulated/downsampled/h5/Spheres-FS5-Augm.h5 --hyperparams /home/hornberger/Projects/MasterarbeitTobias/implementation/Finetuning-Simulated/Simulated-NextStep-60kDecay-10Layers-RegL1.json  --overrideModel $modelFolder  2>&1 | tee -a /home/hornberger/MasterarbeitTobias/logs/Finetuning-Simulated/60kDecaySteps-10layers-RegL1.txt
+(cd $modelFolder && bash /home/hornberger/MasterarbeitTobias/scripts/convertTFEVENTtoCSV.sh delete)
+
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+echo "$dt"
+echo "finished hyperV2"
+
+modelFolder="/home/hornberger/MasterarbeitTobias/models/simulated/NextStep/NextStep-60kDecaySteps-10Layers-RegL2"
+python DNNRegressor-Example.py --training --custom --augment --load /home/hornberger/MasterarbeitTobias/data/simulated/downsampled/h5/Spheres-FS5-Augm.h5 --hyperparams /home/hornberger/Projects/MasterarbeitTobias/implementation/Finetuning-Simulated/Simulated-NextStep-60kDecay-10Layers-RegL2.json  --overrideModel $modelFolder  2>&1 | tee -a /home/hornberger/MasterarbeitTobias/logs/Finetuning-Simulated/60kDecaySteps-10layers-RegL2.txt
+(cd $modelFolder && bash /home/hornberger/MasterarbeitTobias/scripts/convertTFEVENTtoCSV.sh delete)
+
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+echo "$dt"
+echo "finished hyperV3"
+
+
+modelFolder="/home/hornberger/MasterarbeitTobias/models/simulated/NextStep/NextStep-ReLU"
+python DNNRegressor-Example.py --training --custom --augment --save /home/hornberger/MasterarbeitTobias/data/simulated/downsampled/h5/Spheres-FS5-Augm.h5 --hyperparams /home/hornberger/Projects/MasterarbeitTobias/implementation/Finetuning-Simulated/Simulated-NextStep-60kDecay-ReLU.json --overrideModel $modelFolder  2>&1 | tee -a /home/hornberger/MasterarbeitTobias/logs/Finetuning-Simulated/NextStep-60kDecay-ReLU.json
+(cd $modelFolder && bash /home/hornberger/MasterarbeitTobias/scripts/convertTFEVENTtoCSV.sh delete)
+
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+echo "$dt"
+echo "finished hyperV4"
+
